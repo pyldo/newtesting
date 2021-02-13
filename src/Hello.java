a368a1d9c63b12ef9a453392f64fb4b9cb7f56e4
pyldo <2221913240@qq.com>
2021-02-13 09:19:24 +0800

    这是第一次使用ideapush到github好像还不带有些内容

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * @Author zhiye
+ * @date 2021-2-13 0:04
+ */
+public interface Hello {
+    int A = 1;
+    void method1();
+}
---
+++ src/HelloTest.java
@@ -0,0 +1,9 @@
+/**
+ * @Author zhiye
+ * @date 2021-2-13 0:02
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试");
+    }
+}
